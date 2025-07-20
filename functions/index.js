5e7bbc4d0a143c6ecd09435934efdb53c2359436
Dirceu Jr <dirceuu@gmail.com>
2025-07-19 23:02:26 -0300

    add firestore and empty endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { setGlobalOptions } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/https");
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+// for Firestore
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore, FieldValue } = require("firebase-admin/firestore");
+
+// Mandatory for Firestore
+initializeApp();
+const db = getFirestore();
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -26,7 +24,11 @@ setGlobalOptions({ maxInstances: 10 });
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.reset = onRequest(async (request, response) => {
+});
+
+exports.balance = onRequest(async (request, response) => {
+});
+
+exports.event = onRequest(async (request, response) => {
+});
